dd094aabafddfa0b4ade6201b825b7500b9e8b77
Nakadzima <pascal.german@yandex.ru>
2021-10-21 21:22:40 +0600

    update settings

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
